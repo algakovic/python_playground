11b63ae63237ec968703370b9be9214e099711b3
Aleksandar Gakovic <algakovic@gmail.com>
2021-06-02 03:09:54 +0100

    added final input test windows versions assertions with failure comments

--- wintest.py
+++ wintest.py
@@ -14,7 +14,17 @@ for flag in ['-h', '--help']:
     
 assert stdout.stdout.lower().startswith('usage')
 assert stdout.returncode == 0
+
 # --------------------------------------------------
 
 test_usage()
+def test_input():
+    """test for input"""
 
+    for val in ['Universe', 'Multiverse']:
+        for option in ['-n', '--name']:
+            out = subprocess.run(['python', 'tiny1.py', option, val], capture_output=True, text=True)
+    assert out.returncode == 0, "return code != to 0"
+    assert out.stdout.strip() == f'Hello, {val}!', "assertion fail strip value not correct"
+
+test_input()
