31a3b7bd094c3cc6fcbf51b2e29d4991514eeadd
Aleksandar Gakovic <algakovic@gmail.com>
2021-06-02 12:55:40 +0100

    major finding that getoutput and getstatusoutput both work on windows, added code, next commit add those to final input test

--- wintest.py
+++ wintest.py
@@ -4,16 +4,21 @@
 
 import subprocess
 
-
+"""Both of the tests below work""" 
 # --------------------------------------------------
-def test_usage():
-   """usage"""
+#def test_usage():
+ #   """usage"""
+  #  for flag in ['-h', '--help']:
+   #     stdout = subprocess.run(['python', 'hello.py', flag], capture_output=True, text=True)
+    #assert stdout.stdout.lower().startswith('usage')
+    #assert stdout.returncode == 0
 
-for flag in ['-h', '--help']:
-    stdout = subprocess.run(['python', 'tiny1.py', flag], capture_output=True, text=True)
-    
-assert stdout.stdout.lower().startswith('usage')
-assert stdout.returncode == 0
+def test_usage():
+    """usage"""
+    for flag in ['-h', '--help']:
+        rv, statusout = getstatusoutput(f'python {prg} {flag}')
+    assert statusout.lower().startswith('usage')
+    assert rv == 0
 
 # --------------------------------------------------
 
