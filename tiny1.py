73f6af8b2e69de631c98dcc2aba1e6515cfd0ded
Aleksandar Gakovic <algakovic@gmail.com>
2021-06-05 00:08:07 +0100

    make final pylint and flake8 suggestions

--- tiny1.py
+++ tiny1.py
@@ -9,21 +9,26 @@ import argparse
 
 
 def get_args():
-'''
-gets arguments from argparse
-'''
+    '''
+    gets arguments from argparse
+    '''
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar="name", default='World', help='Name to greet')
+    parser.add_argument('-n',
+                        '--name',
+                        metavar="name",
+                        default='World',
+                        help='Name to greet')
     return parser.parse_args()
 
+
 def main():
-'''
-Main function runs script
-'''
+    '''
+    Main function runs script
+    '''
     args = get_args()
     name = args.name
     print("Hello, " + name + "!")
 
+
 if __name__ == '__main__':
     main()
-
