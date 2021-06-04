6880bebf5337c2e26e87716360fb5a18640cfdf5
Aleksandar Gakovic <algakovic@gmail.com>
2021-06-04 23:18:26 +0100

    add function doc strings and more flake 9 / puylint cleanup

--- tiny1.py
+++ tiny1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 '''
 Author: Alek Gakovic
 Email: algakovic@gmail.com
@@ -7,16 +6,24 @@ Purpose: testing
 '''
 
 import argparse
+
+
 def get_args():
+'''
+gets arguments from argparse
+'''
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('-n', '--name', metavar="name", default='World', help='Name to greet')
     return parser.parse_args()
 
 def main():
+'''
+Main function runs script
+'''
     args = get_args()
     name = args.name
-
     print("Hello, " + name + "!")
 
 if __name__ == '__main__':
     main()
+
