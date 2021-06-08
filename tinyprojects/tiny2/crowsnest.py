f5da9be3b87a565e6666a3e49ce4b04f172084f8
Aleksandar Gakovic <algakovic@gmail.com>
2021-06-08 17:19:24 +0100

    remove irrelevant argparse code and add print statement to main. add description, help and metavar as well as positional argument to parser

--- tinyprojects/tiny2/crowsnest.py
+++ tinyprojects/tiny2/crowsnest.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
-Author : Anonymous <Anonymous@ArkSealand>
+Author : ArkSealand>
 Date   : 2021-06-08
-Purpose: Rock the Casbah
+Purpose: Inform the captain
 """
 
 import argparse
@@ -13,38 +13,12 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='name the object sighted',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument('object',
+                        metavar='object',
+                        help='name the object sighted')
 
     return parser.parse_args()
 
@@ -54,17 +28,7 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print(f'Captain theres a {args.object} on the larbord bow')
 
 
 # --------------------------------------------------
