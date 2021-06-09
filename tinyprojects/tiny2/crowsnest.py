c3e1de3e232061f56b23f283a9af7fb921afd516
Aleksandar Gakovic <algakovic@gmail.com>
2021-06-09 12:40:50 +0100

    add consonant and vowel filter to the print statement in tiny2

--- tinyprojects/tiny2/crowsnest.py
+++ tinyprojects/tiny2/crowsnest.py
@@ -7,7 +7,7 @@ Purpose: Inform the captain
 
 import argparse
 
-
+vowels = ('A', 'E', 'I', 'O', 'U')
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
@@ -28,7 +28,11 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    print(f'Captain theres a {args.object} on the larbord bow')
+
+    if args.object.capitalize().startswith(tuple(vowels)):
+        print(f'Ahoy, Captain an {args.object.capitalize()} has been spotted on the larbord bow')
+    else:
+        print(f'Ahoy Captain a {args.object.capitalize()} has been spotted on the Larbord bow!')
 
 
 # --------------------------------------------------
